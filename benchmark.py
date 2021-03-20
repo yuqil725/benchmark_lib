aed6366f6d834346ae34524644e82314ec0d9a8f
Cody Wang <codywang@Codys-MacBook-Pro.local>
2021-03-20 12:02:44 -0400

    download saved model from s3 and unzip

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,12 @@
 from typing import List
 from typing import NamedTuple
+from zipfile import ZipFile
 
+import requests
 import tensorflow as tf
 from tensorflow.keras.applications import VGG16
+
+from benchmark_constants import GPU_STATS
 
 # from tensorflow.keras.applications import InceptionResNetV2
 # from tensorflow.keras.applications import InceptionV3
@@ -11,14 +15,12 @@ from tensorflow.keras.applications import VGG16
 
 # from tensorflow.keras.applications import Xception
 
-GPU_STATS = {"T4": {"bandwidth": 298.08, "cores": 40, "clock": 1590}}
-
 
 class BenchmarkData(NamedTuple):
     data_name: str
     actual_time: float
     raw_model: tf.keras.Model
-    trained_model_path: str
+    trained_model_url: str
     batch_size: int
     optimizer: str
     training_size: int
@@ -37,35 +39,25 @@ BENCHMARK = [
         tf_version="2.4.1",
         benchmark_data=[
             BenchmarkData(
-                data_name="vgg16_input128_batch4_optimizer-sgd",
-                actual_time=41.77,
+                data_name="vgg16_input32_batch4_optimizer-sgd",
+                actual_time=21.59,
                 raw_model=VGG16,
-                trained_model_path="./saved_models/vgg16_input128_batch4_optimizer-sgd",
+                trained_model_url="https://benchmark-models.s3.amazonaws.com/vgg16_input32_batch4_optimizer-sgd.zip",
                 batch_size=4,
                 optimizer="sgd",
                 training_size=5000,
                 validation_split=0.1,
             ),
-            # BenchmarkData(
-            #     data_name="vgg16_input128_batch4_optimizer-adam",
-            #     actual_time=41.77,
-            #     raw_model=VGG16,
-            #     trained_model_path='',
-            #     batch_size=4,
-            #     optimizer='adam',
-            #     training_size=5000,
-            #     validation_split=0.1
-            # ),
-            # BenchmarkData(
-            #     data_name="mobilenet_1_input128_batch4_optimizer-sgd",
-            #     actual_time=15.92,
-            #     raw_model=MobileNet,
-            #     trained_model_path='',
-            #     batch_size=4,
-            #     optimizer='sgd',
-            #     training_size=5000,
-            #     validation_split=0.1
-            # )
+            BenchmarkData(
+                data_name="vgg16_input32_batch4_optimizer-adam",
+                actual_time=24.97,
+                raw_model=VGG16,
+                trained_model_url="https://benchmark-models.s3.amazonaws.com/vgg16_input32_batch4_optimizer-adam.zip",
+                batch_size=4,
+                optimizer="adam",
+                training_size=5000,
+                validation_split=0.1,
+            ),
         ],
     )
 ]
@@ -83,7 +75,9 @@ def run_benchmark(
     for bmconfig in BENCHMARK:
         if bmconfig.gpu_type == gpu_type and bmconfig.tf_version == tf_version:
             for bmdata in bmconfig.benchmark_data:
-                trained_model = tf.keras.models.load_model(bmdata.trained_model_path)
+                trained_model = _download_saved_model_and_load(
+                    bmdata.trained_model_url, bmdata.data_name
+                )
                 parsed_model = parse_cnn_func(trained_model.layers)
                 layer_names, layer_predictions = predictor_func(
                     parsed_model,
@@ -96,7 +90,7 @@ def run_benchmark(
                 iterations = (bmdata.training_size / bmdata.batch_size) * (
                     1 - bmdata.validation_split
                 )
-                predicted_time = sum(layer_predictions) * iterations / 1000
+                predicted_time = round(sum(layer_predictions) * iterations / 1000, 2)
                 results[bmdata.data_name] = {
                     "actual_time": bmdata.actual_time,
                     "predicted_time": predicted_time,
@@ -104,27 +98,11 @@ def run_benchmark(
     return results
 
 
-# train_t4_tf241_hist = {
-#     "gpu_type": "T4",
-#     "tf_version": "2.4.1",
-#     "vgg16": {
-#         "vgg16_input128_batch4_optimizer-sgd": 41.77,
-#         "vgg16_input128_batch4_optimizer-adam": 48.74,
-#     },
-#     "mobilenet_1": {
-#         "mobilenet_1_input128_batch4_optimizer-sgd": 15.43,
-#         "mobilenet_1_input128_batch4_optimizer-adam": 15.92,
-#     },
-#     "xception": {
-#         "xception_input128_batch4_optimizer-sgd": 43.28,
-#         "xception_input128_batch4_optimizer-adam": 46.04,
-#     },
-#     "resnet50v2": {
-#         "resnet50v2_input128_batch4_optimizer-sgd": 38.76,
-#         "resnet50v2_input128_batch4_optimizer-adam": 41.76,
-#     },
-#     "inception_v3": {
-#         "inception_v3_input128_batch4_optimizer-sgd": 37.92,
-#         "inception_v3_input128_batch4_optimizer-adam": 40.53,
-#     },
-# }
+def _download_saved_model_and_load(url: str, model_name):
+    r = requests.get(url)
+    zip_name = f"{model_name}.zip"
+    open(zip_name, "wb").write(r.content)
+    with ZipFile(zip_name, "r") as zipObj:
+        zipObj.extractall()
+        trained_model = tf.keras.models.load_model(model_name)
+        return trained_model
